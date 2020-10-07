8369dd392dfa60a679aff7a7e728d8c446380b7b
Kirah Strandquist <kirahstrandquist@gmail.com>
2020-10-07 10:24:37 -0400

    Added track model communication
    updates occupancy and updates curr block information

--- TrainModel/Train.h
+++ TrainModel/Train.h
@@ -2,26 +2,30 @@
 #define TRAIN_H
 #include "TrainModelUI.h"
 #include <QMainWindow>
+#include <chrono>
+#include <cstdint>
 using namespace std;
 
 class Train
 {
 public:
     int numCars;
-    uint16_t trackCircuitData;
+    uint64_t trackCircuitData;
     double power;
     double oldPos;
     chrono::steady_clock::time_point oldTime;
     double currVel;
     double currPower;
     double blockDist = 50;
-    int currBlock;
+    double blockGrade = 0;
+    int blockNum;
+
 
     Train(int, int, char **);
     //Train(int, char);
     void setPower(double newPower);             //Called by train controller to set power
     void getTrackCircuit(int block);       //Get curr track signal from Track Model when new block
-    uint16_t sendTrackCircuit();                //Train controller can call to get curr track signal
+    uint64_t sendTrackCircuit();                //Train controller can call to get curr track signal
     void updateTrackInfo();                     //Will update block information
     double getCurrentVelocity();                //Called by train controller to get curr velocity
     double getCurrentPosition();                //will return current position
