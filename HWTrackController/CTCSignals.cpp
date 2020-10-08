2ec83e0665158ba1ae9f6debce887780ac3fd19e
marmysl <54610728+marmysl@users.noreply.github.com>
2020-10-08 06:26:54 -0400

    compatible with ctc

--- HWTrackController/CTCSignals.cpp
+++ HWTrackController/CTCSignals.cpp
@@ -5,17 +5,6 @@
 #include "CTCSignals.hpp"
 using namespace std;
 
-void CTCSignals :: setSignal(int b, float s[], float a[]) {
-	for (int i = 0; i < 15; i++){
-		speedCTC.push_back(s[i]);
-	}
-	for (int i = 0; i < 15; i++){
-		authCTC.push_back(a[i]);
-	}
-
-	destinationBlock = b;
-}
-
 std::vector<float> CTCSignals :: getSpeed() {
 	return speedCTC;
 }
