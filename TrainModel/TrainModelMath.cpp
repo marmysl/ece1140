d4a9c36fa4fefe7f4116f2df60818249d8481010
Kirah Strandquist <kirahstrandquist@gmail.com>
2020-10-12 20:18:58 -0400

    train model UI
    shows AND updates!! yay!!

--- TrainModel/TrainModelMath.cpp
+++ TrainModel/TrainModelMath.cpp
@@ -18,33 +18,3 @@ double TrainModelMath::calcVelocity(double power) {
     return 20;
 }
 
-/*
-TrainModelMath();
-static double travelledDist(double time, double velocity);
-static double updatePosition(double oldPos, double change);
-static double calcVelocity(double power);
-=======
-/*
-class TrainModelMath
-{
-    public:
-        TrainModelMath()
-        {
-            //empty constructor
-        }
-
-        static double travelledDist(double time, double velocity){
-            double dist = velocity * time;
-            return dist;
-        }
-
-        static double updatePosition(double oldPos, double change){
-            return oldPos+change;
-        }
-
-        static double calcVelocity(double power){
-            return 20;
-        }
-};
-*/
-
